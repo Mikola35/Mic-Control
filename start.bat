6d02a6ffcccaf724fd37f0b8aba86e1d13a76f8a
Nick M <nik@mamadaevv.ru>
2025-04-13 23:27:25 +0200

    Обновлен скрипт запуска start.bat: теперь программа запускается в свернутом виде, улучшен вывод сообщений об ошибках.

--- start.bat
+++ start.bat
@@ -1,11 +1,8 @@
 @echo off
 chcp 65001 > nul
-title Mic Control
 echo Starting Mic Control...
-.venv\Scripts\python.exe mic_control.py
+start /min .venv\Scripts\python.exe mic_control.py
 if errorlevel 1 (
-    echo.
     echo Program failed to start. Check the error messages above.
-    echo Press any key to exit...
     pause > nul
 ) 
